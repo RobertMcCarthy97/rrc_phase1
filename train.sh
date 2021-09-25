9ded22bf0a823938571deeaa5f5e7246e28a8a25
RobertMcCarthy97 <robert.mccarthy@ucdconnect.ie>
2021-09-25 18:34:07 +0100

    Train with x-y HER + z rewards

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=normalHERzrew
+#SBATCH --job-name=xyHERzrew
 # specify number of nodes 
 #SBATCH -N 1
 
@@ -21,5 +21,5 @@ module load singularity/3.5.2
 # The following commands are informed by:
 # https://people.tuebingen.mpg.de/felixwidmaier/rrc2021/singularity.html#build-and-run-code-in-singularity
 
-singularity run /home/people/16304643/robochallenge/user_image.sif mpirun -np 8 python3 train.py --noisy-resets=1 --noise-level=1 --difficulty=3 --trajectory-aware=0 --ep-len=90 --steps-per-goal=30 --step-size=50 --z-reward=1 --z-scale=20 --xy-only=0 --simtoreal=1 --domain-randomization=0 --obs-type='default' --exp-dir='normalHERzrew' --n-epochs=300 2>&1 | tee normalHERzrew.log
+singularity run /home/people/16304643/robochallenge/user_image.sif mpirun -np 8 python3 train.py --noisy-resets=1 --noise-level=1 --difficulty=3 --trajectory-aware=0 --ep-len=90 --steps-per-goal=30 --step-size=50 --z-reward=1 --z-scale=20 --xy-only=1 --simtoreal=1 --domain-randomization=0 --obs-type='default' --exp-dir='xyHERzrew' --n-epochs=300 2>&1 | tee xyHERzrew.log
 
